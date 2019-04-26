b34800345f55769836e20697ce2d2d4df4523a24
WHBJ <bevansrealtors@gmail.com>
2019-04-26 13:25:56 -0500

    Updates

---
+++ precache-manifest.fe2f4ea65aa2e4405bbed971c274c865.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "470c7c22369111f771cad6fd36e58c01",
+    "url": "/williambevans/click-game/index.html"
+  },
+  {
+    "revision": "e249dededad8d933ebd7",
+    "url": "/williambevans/click-game/static/css/main.01e0e383.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/williambevans/click-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "e249dededad8d933ebd7",
+    "url": "/williambevans/click-game/static/js/main.1bdf43c5.chunk.js"
+  },
+  {
+    "revision": "5eef9727d5b19fac1047",
+    "url": "/williambevans/click-game/static/js/runtime~main.d50d228a.js"
+  }
+]);
